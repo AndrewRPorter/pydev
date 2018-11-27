2485ceaf01b304f42dcafe2bc7b8313f9f79ef80
AndrewRPorter <pavelow53.andrew@gmail.com>
2018-11-27 11:00:07 -0500

    Andrew: added atom repository

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Check if running KDE or Gnome
-ps -ef | grep gnome
+# Add atom package repository
+curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 
+# Refresh repository and install updates for installed packages
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # Install repository software
-sudo apt-get install -y vim python3-pip gnome-terminal
+sudo apt-get install -y atom vim python3-pip gnome-terminal
 
 # Install python modules
 pip3 install --user -r requirements.txt
