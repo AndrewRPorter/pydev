80c314c13f4a1496b7c15498f2e71fda239a3de8
AndrewRPorter <pavelow53.andrew@gmail.com>
2018-11-29 21:03:28 -0500

    Andrew: moved atom install statement and moved pip install to script so it could be executed with wget

--- install.sh
+++ install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# Add atom package repository
-curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-
 # Refresh repository and install updates for installed packages
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install repository software
-sudo apt-get install -y git atom vim python3-pip gnome-terminal unzip
+sudo apt-get install -y git vim python3-pip gnome-terminal unzip
+
+# Add atom package repository
+curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+
+# Install atom
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y atom
 
 # Install atom packages
 apm install autocomplete-python
-
-# Install Node.js Version – 7.x
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# Install npm modules
-npm install -g tldr
 
 # Install exa software
 wget -P ~ "https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip"
@@ -33,9 +31,15 @@ sudo dpkg -i ~/bat-musl_0.9.0_amd64.deb
 rm ~/bat-musl_0.9.0_amd64.deb
 
 # Install python modules
-pip3 install --user -r requirements.txt
+pip3 install --user requests django pyyaml boto3 gunicorn jinja2 black tldr
 
 # Apply aliases to bashrc file
 echo "alias cat='bat'" >> ~/.bashrc
 echo "alias man='tldr'" >> ~/.bashrc
 echo "alias ls='/usr/bin/exa-linux-x86_64 --long'" >> ~/.bashrc
+
+# Apply Vim formatting
+touch ~/.vimrc
+echo "set background=dark" >> ~/.vimrc
+echo "set number" >> ~/.vimrc
+echo "colorscheme torte" >> ~/.vimrc
