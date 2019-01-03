75fef8be8acaa364edf15fbb00f96f3ecbcbc19f
AndrewRPorter <pavelow53.andrew@gmail.com>
2019-01-03 17:36:08 -0500

    added tmux, spacevim and flask python module. Also added some more vim configurations

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 # Refresh repository and install updates for installed packages
 sudo apt-get update
-sudo apt-get upgrade -y
 
 # Install repository software
-sudo apt-get install -y git vim python3-pip gnome-terminal unzip tldr curl cloc
+sudo apt-get install -y git vim python3-pip gnome-terminal unzip tldr curl cloc tmux
+
+# Install space-vim
+curl -sLf https://spacevim.org/install.sh | bash
 
 # Add atom package repository
 curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
@@ -17,6 +19,7 @@ sudo apt-get install -y atom
 
 # Install atom packages
 apm install autocomplete-python
+apm install ftp-remote-edit
 
 # Install exa software
 wget -P ~ "https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip"
@@ -30,7 +33,7 @@ sudo dpkg -i ~/bat-musl_0.9.0_amd64.deb
 rm ~/bat-musl_0.9.0_amd64.deb
 
 # Install python modules
-pip3 install --user requests django pyyaml boto3 gunicorn jinja2 black virtualenv
+pip3 install --user requests django flask pyyaml boto3 gunicorn jinja2 black virtualenv
 
 # Apply aliases to bashrc file
 echo "alias cat='bat'" >> ~/.bashrc
@@ -38,7 +41,7 @@ echo "alias man='tldr'" >> ~/.bashrc
 echo "alias ls='/usr/bin/exa-linux-x86_64 --long'" >> ~/.bashrc
 
 # Apply Vim formatting
-touch ~/.vimrc
-echo "set background=dark" >> ~/.vimrc
-echo "set number" >> ~/.vimrc
-echo "colorscheme torte" >> ~/.vimrc
+echo "set background=dark" >> ~/.vimrc_back
+echo "set number" >> ~/.vimrc_back
+echo "set incsearch" >> ~/.vimrc_back
+echo "colorscheme torte" >> ~/.vimrc_back
