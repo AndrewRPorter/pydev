47082f42716ec00fb07be261262f4baf39c8eaea
AndrewRPorter <pavelow53.andrew@gmail.com>
2019-01-16 14:15:36 -0500

    Removed atom from installation script. Added ripgrep and isort

--- README.rst
+++ README.rst
@@ -11,18 +11,26 @@ Usage
 
         wget -O - https://raw.githubusercontent.com/AndrewRPorter/pyenv/master/install.sh | bash
 
+ or
+
+ .. code:: shell
+
+        git clone git@github.com:AndrewRPorter/pydev.git      
+        cd pydev
+        ./install.sh
+
 Included Software
 -----------------
 
-- atom: Open source text editor with git integration
 - pip3: Tool for installing python packages
 - vim: CLI text editor
 - git: Version control
 - unzip: Easy unzipping software to manage .zip files from the CLI
 - gnome-terminal: terminal emulator for GNOME desktop enviornment
 - tldr: Simplfied MAN pages
-- bat: Modern replacement of ``cat``
+- bat: Modern replacement of ``cat`` written in Rust
 - exa: Replacement for ``ls`` written in Rust
+- ripgrep: Replacement for ``grep`` written in Rust
 - curl: CLI tool for transferring data
 - cloc: Tool to count lines of code
 
@@ -34,9 +42,10 @@ Python Packages
 - PYYaml: YAML parser and emitter for Python
 - boto3: Amazon Web Services (AWS) Software Development Kit (SDK) for Python
 - gunicorn: Python WSGI HTTP Server for UNIX
-- jinaj2: Template engine
 - black: Python code formatter
 - virtualenv: A tool for creating isolated 'virtual' python environments
+- isort: A Python utility to sort imports
+- pytest: Python testing framework
 
 Notes
 -----
--- install.sh
+++ install.sh
@@ -9,18 +9,6 @@ sudo apt-get install -y git vim python3-pip gnome-terminal unzip tldr curl cloc 
 # Install space-vim
 curl -sLf https://spacevim.org/install.sh | bash
 
-# Add atom package repository
-curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
-
-# Install atom
-sudo apt-get update
-sudo apt-get install -y atom
-
-# Install atom packages
-apm install autocomplete-python
-apm install ftp-remote-edit
-
 # Install exa software
 wget -P ~ "https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip"
 unzip ~/exa-linux-x86_64-0.8.0.zip
@@ -32,13 +20,19 @@ wget -P ~ "https://github.com/sharkdp/bat/releases/download/v0.9.0/bat-musl_0.9.
 sudo dpkg -i ~/bat-musl_0.9.0_amd64.deb
 rm ~/bat-musl_0.9.0_amd64.deb
 
+# Install ripgrep software
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
+sudo dpkg -i ~/ripgrep_0.10.0_amd64.deb
+rm ~/ripgrep_0.10.0_amd64.deb
+
 # Install python modules
-pip3 install --user requests django flask pyyaml boto3 gunicorn jinja2 black virtualenv
+pip3 install --user requests django flask pyyaml boto3 gunicorn black isort virtualenv pytest
 
 # Apply aliases to bashrc file
 echo "alias cat='bat'" >> ~/.bashrc
 echo "alias man='tldr'" >> ~/.bashrc
 echo "alias ls='/usr/bin/exa-linux-x86_64 --long'" >> ~/.bashrc
+echo "alias grep='rg'" >> ~/.bashrc
 
 # Apply Vim formatting
 echo "set background=dark" >> ~/.vimrc_back
